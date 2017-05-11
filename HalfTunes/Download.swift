16965b35677092d8ed53197859c37079c424e98d
Nguyễn Anh Kiệt <kietnguyen0814@gmail.com>
2017-05-11 18:09:44 +0700

    Initial Commit

---
+++ HalfTunes/Download.swift
@@ -0,0 +1,22 @@
+//
+//  Download.swift
+//  HalfTunes
+//
+//  Created by Kiet Nguyen on 5/11/17.
+//  Copyright © 2017 Ken Toh. All rights reserved.
+//
+
+import Foundation
+class Download: NSObject {
+    
+    var url: String
+    var isDownloading = false
+    var progress: Float = 0.0
+    
+    var downloadTask: URLSessionDownloadTask?
+    var resumeData: NSData?
+    
+    init(url: String) {
+        self.url = url
+    }
+}
